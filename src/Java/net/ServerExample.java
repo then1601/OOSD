81d26fa3d73323f6560bd762da9f43386ac1f34f
then1601 <truongquangthang1601@gmail.com>
2026-01-28 08:42:50 +0700

    first commit

---
+++ src/Java/net/ServerExample.java
@@ -0,0 +1,61 @@
+package Java.net;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+ 
+public class ServerExample extends Thread {
+    private ServerSocket serverSocket;
+ 
+    public ServerExample(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        serverSocket.setSoTimeout(30000);
+    }
+ 
+    public void run() {
+        while (true) {
+            try {
+                System.out.println("Waiting for client on port "
+                        + serverSocket.getLocalPort() + "...");
+                Socket server = serverSocket.accept();
+ 
+                System.out.println("Just connected to "
+                        + server.getRemoteSocketAddress());
+                DataInputStream in = new DataInputStream(server.getInputStream());
+ 
+                System.out.println(in.readUTF());
+                DataOutputStream out = new DataOutputStream(
+                        server.getOutputStream());
+                out.writeUTF("Thank you for connecting to "
+                        + server.getLocalSocketAddress() + "\nGoodbye!");
+                server.close();
+ 
+            } catch (SocketTimeoutException s) {
+                System.out.println("Socket timed out!");
+                break;
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
+ 
+    /**
+     * main
+     * 
+     * @author viettuts.vn
+     * @param args
+     */
+    public static void main(String[] args) {
+        int port = Integer.parseInt(args[0]);
+        try {
+            Thread t = new ServerExample(port);
+            t.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
